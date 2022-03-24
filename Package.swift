c9e962b5c954f82b5cf6f53c7cbe4d874e0a9862
mdarius13 <mdarius13@icloud.com>
2022-03-24 18:59:10 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,26 +3,39 @@
 
 import PackageDescription
 
+/// Base58+ Package
 let package = Package(
+    /// Package Name
     name: "Base58-Plus",
+    
+    /// Package Products
+    /// Products define the executables and libraries a package produces, and make them visible to other packages.
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "Base58-Plus",
             targets: ["Base58-Plus"]),
     ],
+    /// Product Dependencies
+    /// Dependencies declare other packages that this package depends on.
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/attaswift/BigInt.git", from: "5.3.0")
     ],
+    
+    /// Package Targets
+    /// Targets are the basic building blocks of a package. A target can define a module or a test suite.
+    /// Targets can depend on other targets in this package, and on products in packages this package depends on.
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        /// Main library
         .target(
             name: "Base58-Plus",
-            dependencies: []),
+            dependencies: ["BigInt"],
+            path: "./Sources/"
+        ),
+        
+        /// Unit tests
         .testTarget(
-            name: "Base58-PlusTests",
-            dependencies: ["Base58-Plus"]),
+            name: "Tests",
+            dependencies: ["Base58-Plus"],
+            path: "./Tests/")
     ]
 )
